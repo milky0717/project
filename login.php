5c84e19286f61de9a590ee347197d850d2a9bd8e
Ryouya Ozawa <ozawar88@gmail.com>
2024-11-28 16:31:44 +0900

    First commit

---
+++ login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ログイン</title>
+    </head>
+    <body>
+        //アカウント名とパスワードをデータベースから参照
+        //アカウント名が八文字ならreader.phpにその他はfollowers.phpに
+    </body>
+</html>
---
+++ search.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>データ参照</title>
+    </head>
+    <body>
+        <h1>データ参照</h1>
+        //user検索
+            <label for="id">ID:</label>
+            <input type="text" id="id" name="id">
+            <button type="search">検索する</button>
+        //新規登録
+        <label for="newid">ID:</label>
+        <input type="text" id="newid" name="newid">
+        <button type="new">新規登録</button>
+    </body>
+</html>
+<?php
+$conn =null;
+try{
+    $conn = new
+}catch(){
+
+}
+?>
